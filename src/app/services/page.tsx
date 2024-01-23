3c29c6db67dcb231ba2929bfdb8d8f5d4ce969ff
kigos21 <decastrojohndaniel@gmail.com>
2024-01-23 17:10:38 +0800

    update style

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -3,12 +3,13 @@ import ServicePillItem from '@/components/services/ServicePillItem';
 
 export default function Page() {
   return (
-    <div className="flex flex-col gap-12 px-[7%] py-4">
-      <h1>Our Services</h1>
+    <div className="flex flex-col gap-12 px-[7%] py-12">
+      <h1 className="text-center">Our Services</h1>
 
       {/* <div className="grid grid-cols-2 gap-8"> */}
       <div className="flex justify-center gap-8">
-        <div className="flex flex-col gap-4 rounded-3xl bg-otb-yellow p-8">
+        <div className="flex flex-col gap-4 rounded-3xl bg-otb-yellow p-8 shadow-2xl">
+          {/* REGULAR RATES */}
           <h2 className="text-center">Regular Rates</h2>
 
           <ul className="grid grid-cols-2 gap-4">
@@ -31,21 +32,25 @@ export default function Page() {
                   {list.title}
                 </span>
 
-                <ul className="flex flex-col items-center">
+                <ul className="flex flex-col items-center text-sm">
                   {list.items.map((item) => (
                     <li key={item}>{item}</li>
                   ))}
 
                   {list.price && (
-                    <li className="font-semibold italic">{list.price}</li>
+                    <li className="text-sm font-semibold italic">
+                      {list.price}
+                    </li>
                   )}
                 </ul>
               </div>
             ))}
           </div>
+          {/* END OF REGULAR RATES */}
         </div>
 
-        <div className="flex flex-col gap-4 rounded-3xl bg-otb-yellow p-8">
+        <div className="flex flex-col gap-4 rounded-3xl bg-otb-yellow p-8 shadow-2xl">
+          {/* SPECIAL PROMOS */}
           <h2 className="text-center">Special Promos</h2>
 
           <ul className="flex flex-col gap-4">
@@ -53,7 +58,10 @@ export default function Page() {
               <ServicePillItem key={promo.title} {...promo} />
             ))}
           </ul>
+          {/* END OF SPECIAL PROMOS */}
         </div>
+
+        <div></div>
       </div>
     </div>
   );
--- src/components/services/ServicePillItem.tsx
+++ src/components/services/ServicePillItem.tsx
@@ -2,7 +2,7 @@ import { ServicePill } from '@/types';
 
 export default function ServicePillItem({ title, price }: ServicePill) {
   return (
-    <li className="rounded-full bg-otb-blue px-4 py-2 text-center font-semibold">
+    <li className="rounded-full bg-otb-blue px-4 py-2 text-center font-semibold uppercase">
       {`${title} - ${price}`}
     </li>
   );
