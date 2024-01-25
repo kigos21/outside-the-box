dcf4d2f754cbef7c50ddbf9c1594d61b4733ee92
kigos21 <decastrojohndaniel@gmail.com>
2024-01-25 19:51:09 +0800

    formatted code

--- src/app/login/forgot.tsx
+++ src/app/login/forgot.tsx
@@ -1,57 +1,95 @@
-import Link from "next/link";
-import Image from "next/image";
-import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal, PromiseLikeOfReactNode } from "react";
+import Link from 'next/link';
+import Image from 'next/image';
+import {
+  ReactElement,
+  JSXElementConstructor,
+  ReactNode,
+  ReactPortal,
+  PromiseLikeOfReactNode,
+} from 'react';
 
-export default function Forget(){
-    return(
-        <div className= "flex justify-center items-center h-screen">
-        <div className="rounded-md w-96 p-6 shadow-lg m-3 bg-otb-yellow grid justify-items-center gap-3 content-evenly">
-                <div>        
-                    <Link href={'/'}>
-                    <Image
-                    src="/otb-logo-cropped.jpg"
-                    width={300}
-                    height={50}
-                    alt="Outside the Box Logo"
-                    />
-                    </Link>
-                </div>
-                <Forgot
-                    title="Forget Password"
-                    pholder="Username"/>
-        </div>
-        </div>
-    );
-}
-
-function Forgot(props: { title: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; pholder: string | undefined; }){
-    return(
+export default function Forget() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="m-3 grid w-96 content-evenly justify-items-center gap-3 rounded-md bg-otb-yellow p-6 shadow-lg">
         <div>
-            <h2>{props.title}</h2>
-            <div className="mt-3">
-            <input type="text" id="username" className="rounded-lg p-2" placeholder={props.pholder}/>
-            <br></br>
-                <div className="flex justify-around mt-5">            
-                    <button className="rounded px-5 bg-blue-300">Submit</button>
-                    <button className="rounded px-5 bg-blue-300">Cancel</button>
-                </div>
-            </div>
+          <Link href={'/'}>
+            <Image
+              src="/otb-logo-cropped.jpg"
+              width={300}
+              height={50}
+              alt="Outside the Box Logo"
+            />
+          </Link>
         </div>
-    );
-}
-
-function OTP(props: { title: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; pholder: string | undefined; }){
-    return(
-        <div>
-        <h2>{props.title}</h2>
-        <div className="mt-3">
-        <input type="text" id="username" className="rounded-lg p-2" placeholder={props.pholder}/>
-        <br></br>
-            <div className="flex justify-around mt-5">            
-                <button className="rounded px-5 bg-blue-300">Submit</button>
-                <button className="rounded px-5 bg-blue-300">Cancel</button>
-            </div>
-        </div>
+        <Forgot title="Forget Password" pholder="Username" />
+      </div>
     </div>
-    );
+  );
+}
+
+function Forgot(props: {
+  title:
+    | string
+    | number
+    | boolean
+    | ReactElement<any, string | JSXElementConstructor<any>>
+    | Iterable<ReactNode>
+    | ReactPortal
+    | PromiseLikeOfReactNode
+    | null
+    | undefined;
+  pholder: string | undefined;
+}) {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <div className="mt-3">
+        <input
+          type="text"
+          id="username"
+          className="rounded-lg p-2"
+          placeholder={props.pholder}
+        />
+        <br></br>
+        <div className="mt-5 flex justify-around">
+          <button className="rounded bg-blue-300 px-5">Submit</button>
+          <button className="rounded bg-blue-300 px-5">Cancel</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function OTP(props: {
+  title:
+    | string
+    | number
+    | boolean
+    | ReactElement<any, string | JSXElementConstructor<any>>
+    | Iterable<ReactNode>
+    | ReactPortal
+    | PromiseLikeOfReactNode
+    | null
+    | undefined;
+  pholder: string | undefined;
+}) {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <div className="mt-3">
+        <input
+          type="text"
+          id="username"
+          className="rounded-lg p-2"
+          placeholder={props.pholder}
+        />
+        <br></br>
+        <div className="mt-5 flex justify-around">
+          <button className="rounded bg-blue-300 px-5">Submit</button>
+          <button className="rounded bg-blue-300 px-5">Cancel</button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,38 +1,51 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-
-export default function Login(){
-    return(
-        <div className= "flex justify-center items-center h-screen">
-        <div className="rounded-md w-96 p-6 shadow-lg m-3 bg-otb-yellow grid justify-items-center gap-3 content-evenly">
-                <div>        
-                    <Link href={'/'}>
-                    <Image
-                    src="/otb-logo-cropped.jpg"
-                    width={300}
-                    height={50}
-                    alt="Outside the Box Logo"
-                    />
-                    </Link>
-                </div>
-            <div className="mt-3">
-                <input type="text" id="username" className="rounded-lg p-2" placeholder="Username"/>
-            </div>
-            <div className="mt-3">
-                <input type="password" id="password" className="rounded-lg p-2" placeholder="Password"/>
-            </div>
-            <div>
-            <div className="flex float-right mb-3 "><Link href={'/'}>Forget Password</Link></div>
-            <br></br>
-            <div className="flex justify-around my-3">            
-                <button className="rounded px-5 bg-blue-300">Submit</button>
-                <button className="rounded px-5 bg-blue-300">Cancel</button>
-            </div>
-                <div className="flex">Don&apos;t have an account yet? <h5 className="text-blue-300 font-bold mx-1">Register</h5> here</div>
-            </div>
+export default function Login() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="m-3 grid w-96 content-evenly justify-items-center gap-3 rounded-md bg-otb-yellow p-6 shadow-lg">
+        <div>
+          <Link href={'/'}>
+            <Image
+              src="/otb-logo-cropped.jpg"
+              width={300}
+              height={50}
+              alt="Outside the Box Logo"
+            />
+          </Link>
         </div>
-
+        <div className="mt-3">
+          <input
+            type="text"
+            id="username"
+            className="rounded-lg p-2"
+            placeholder="Username"
+          />
         </div>
-    );
+        <div className="mt-3">
+          <input
+            type="password"
+            id="password"
+            className="rounded-lg p-2"
+            placeholder="Password"
+          />
+        </div>
+        <div>
+          <div className="float-right mb-3 flex ">
+            <Link href={'/'}>Forget Password</Link>
+          </div>
+          <br></br>
+          <div className="my-3 flex justify-around">
+            <button className="rounded bg-blue-300 px-5">Submit</button>
+            <button className="rounded bg-blue-300 px-5">Cancel</button>
+          </div>
+          <div className="flex">
+            Don&apos;t have an account yet?{' '}
+            <h5 className="mx-1 font-bold text-blue-300">Register</h5> here
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
