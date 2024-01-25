8fee67d70c40750a455454a822a4df3e37b3d88c
kigos21 <decastrojohndaniel@gmail.com>
2024-01-25 21:46:50 +0800

    reverted landing page

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -1,10 +1,7 @@
-import Login from './login/page';
-import Forget from './login/forgot';
-
 export default function Home() {
   return (
     <div className="px-[7%] py-4">
-      <Login />
+      <h1>Home</h1>
     </div>
   );
 }
