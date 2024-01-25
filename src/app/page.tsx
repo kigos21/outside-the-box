2858d581a6dd07990e1d2275d0274b49f912e91a
kigos21 <decastrojohndaniel@gmail.com>
2024-01-25 19:51:40 +0800

    optimized markup structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,8 @@ import Forget from './login/forgot';
 
 export default function Home() {
   return (
-<!--     <main>
-      <link href="./globals.css" rel="stylesheet"></link>
-      <h1> <Login/> </h1>
-    </main> -->
-    
-    <div className="px-[7%] py-4>
-      <link href="./globals.css" rel="stylesheet" />
-      <h1> <Login/> </h1>
+    <div className="px-[7%] py-4">
+      <Login />
     </div>
   );
 }
