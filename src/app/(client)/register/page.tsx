3ccb43764244127d305e362fba9bc7b5d3a8b31b
kigos21 <decastrojohndaniel@gmail.com>
2024-02-04 15:39:18 +0800

    form validation and submit handler for POST request for /api/register

--- src/app/(client)/register/page.tsx
+++ src/app/(client)/register/page.tsx
@@ -1,9 +1,35 @@
 'use client';
 
+import OTPForm from '@/components/OTPForm';
+import { RegisterFormBody } from '@/types';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
 export default function Register() {
+  const [isFormValid, setIsFormValid] = useState<boolean>(false);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<RegisterFormBody>();
+  const onSubmit: SubmitHandler<RegisterFormBody> = (data) => {
+    console.log(data);
+    // setIsFormValid(true);
+  };
+
+  const [otp, setOTP] = useState<string>('');
+  const handleOTPChange = (value: string) => {
+    setOTP(value);
+  };
+  const handleOTPSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(otp);
+  };
+
   return (
     <div className="flex min-h-[85dvh] items-center justify-center">
       <div className="my-32 flex flex-col items-center justify-center gap-8 rounded-3xl bg-otb-yellow px-16 py-12 shadow-2xl">
@@ -15,96 +41,231 @@ export default function Register() {
         />
 
         <div className="w-full">
-          <form
-            method="post"
-            onSubmit={(e) => e.preventDefault()}
-            className="flex flex-col gap-4"
-          >
-            <input
-              type="text"
-              name="username"
-              id="username"
-              required
-              placeholder="Username e.g. joe123"
-              className="rounded-full border border-gray-300 px-6 py-4"
-            />
-            <input
-              type="password"
-              name="password"
-              id="password"
-              required
-              placeholder="Password"
-              className="rounded-full border border-gray-300 px-6 py-4"
-            />
-            <input
-              type="password"
-              name="confirmPassword"
-              id="confirmPassword"
-              required
-              placeholder="Confirm Password"
-              className="rounded-full border border-gray-300 px-6 py-4"
-            />
-            <input
-              type="text"
-              name="firstName"
-              id="firstName"
-              required
-              placeholder="First name"
-              className="rounded-full border border-gray-300 px-6 py-4"
-            />
-            <input
-              type="text"
-              name="lastName"
-              id="lastName"
-              required
-              placeholder="Last name"
-              className="rounded-full border border-gray-300 px-6 py-4"
-            />
-            <select
-              name="occupation"
-              id="occupation"
-              className="rounded-full border border-gray-300 px-5 py-4"
+          {!isFormValid ? (
+            <form
+              method="post"
+              onSubmit={handleSubmit(onSubmit)}
+              className="flex max-w-md flex-col gap-4"
             >
-              <option hidden>Occupation</option>
-              <option value="student" className="p-6">
-                Student
-              </option>
-              <option value="working-student">Working Student</option>
-              <option value="professional">Professional</option>
-            </select>
-            <input
-              type="text"
-              name="affiliation"
-              id="affiliation"
-              required
-              placeholder="Affiliation"
-              className="rounded-full border border-gray-300 px-6 py-4"
-            />
-            <input
-              type="tel"
-              name="mobileNumber"
-              id="mobileNumber"
-              required
-              placeholder="Mobile Number"
-              className="rounded-full border border-gray-300 px-6 py-4"
-            />
-
-            <div className="mt-4 flex w-full flex-col gap-4">
-              <button className="rounded-full bg-otb-blue px-6 py-4 font-semibold uppercase shadow-md transition-all hover:bg-black hover:text-white hover:shadow-none">
-                Register
-              </button>
-
-              <span className="block text-center text-sm">
-                Already have an account?{' '}
-                <Link
-                  href="/login"
-                  className="font-bold text-otb-blue underline shadow-sm"
+              <input
+                type="text"
+                id="username"
+                required
+                placeholder="Username e.g. joe123"
+                className="rounded-full border border-gray-300 px-6 py-4"
+                {...register('username', {
+                  required: true,
+                  minLength: 3,
+                  maxLength: 50,
+                  validate: {
+                    valid: (input) => /^[a-zA-Z0-9]{3,}$/.test(input),
+                  },
+                })}
+              />
+              {errors.username && (
+                <p
+                  role="alert"
+                  className="mt-[-0.75rem] px-6 text-xs text-red-500"
                 >
-                  Login
-                </Link>
-              </span>
-            </div>
-          </form>
+                  This field is required, should only contain alphanumeric
+                  characters, and is atleast 3 characters long.
+                </p>
+              )}
+
+              <input
+                type="password"
+                id="password"
+                required
+                placeholder="Password"
+                className="rounded-full border border-gray-300 px-6 py-4"
+                {...register('password', {
+                  required: true,
+                  minLength: 8,
+                  validate: {
+                    strength: (input) =>
+                      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+                        input,
+                      ),
+                  },
+                })}
+              />
+              {errors.password && (
+                <p
+                  role="alert"
+                  className="mt-[-0.75rem] px-6 text-xs text-red-500"
+                >
+                  This field is required, atleast 8 characters long, and include
+                  at least one lowercase letter, one uppercase letter, one
+                  digit, and one special character [@, $, !, %, *, ?, &].
+                </p>
+              )}
+
+              <input
+                type="password"
+                id="confirmPassword"
+                required
+                placeholder="Confirm Password"
+                className="rounded-full border border-gray-300 px-6 py-4"
+                {...register('confirmPassword', {
+                  required: true,
+                  validate: {
+                    match: (input) => input === watch('password'),
+                  },
+                })}
+              />
+              {errors.confirmPassword?.type === 'required' && (
+                <p
+                  role="alert"
+                  className="mt-[-0.75rem] px-6 text-xs text-red-500"
+                >
+                  This field is required.
+                </p>
+              )}
+              {errors.confirmPassword?.type === 'match' && (
+                <p
+                  role="alert"
+                  className="mt-[-0.75rem] px-6 text-xs text-red-500"
+                >
+                  Passwords do not match.
+                </p>
+              )}
+
+              <input
+                type="text"
+                id="firstName"
+                required
+                placeholder="First name"
+                className="rounded-full border border-gray-300 px-6 py-4"
+                {...register('firstName', {
+                  required: true,
+                })}
+              />
+              {errors.firstname && (
+                <p
+                  role="alert"
+                  className="mt-[-0.75rem] px-6 text-xs text-red-500"
+                >
+                  This field is required.
+                </p>
+              )}
+
+              <input
+                type="text"
+                id="lastName"
+                required
+                placeholder="Last name"
+                className="rounded-full border border-gray-300 px-6 py-4"
+                {...register('lastName', {
+                  required: true,
+                })}
+              />
+              {errors.lastname && (
+                <p
+                  role="alert"
+                  className="mt-[-0.75rem] px-6 text-xs text-red-500"
+                >
+                  This field is required.
+                </p>
+              )}
+
+              <select
+                id="occupation"
+                className="rounded-full border border-gray-300 px-5 py-4"
+                defaultValue={''}
+                {...register('occupation', {
+                  required: true,
+                  validate: {
+                    validEnum: (option) =>
+                      option === 'student' ||
+                      option === 'working-student' ||
+                      option === 'professional',
+                  },
+                })}
+              >
+                <option hidden>Occupation</option>
+                <option value="student" className="p-6">
+                  Student
+                </option>
+                <option value="working-student">Working Student</option>
+                <option value="professional">Professional</option>
+              </select>
+              {errors.occupation && (
+                <p
+                  role="alert"
+                  className="mt-[-0.75rem] px-6 text-xs text-red-500"
+                >
+                  This field is required.
+                </p>
+              )}
+
+              <input
+                type="text"
+                id="affiliation"
+                required
+                placeholder="Affiliation"
+                className="rounded-full border border-gray-300 px-6 py-4"
+                {...register('affiliation', {
+                  required: true,
+                })}
+              />
+              {errors.affiliation && (
+                <p
+                  role="alert"
+                  className="mt-[-0.75rem] px-6 text-xs text-red-500"
+                >
+                  This field is required.
+                </p>
+              )}
+
+              <input
+                type="tel"
+                id="mobileNumber"
+                required
+                placeholder="Mobile Number"
+                className="rounded-full border border-gray-300 px-6 py-4"
+                {...register('mobileNumber', {
+                  required: true,
+                  validate: {
+                    isNumber: (tel) => /^\d{11}$/.test(tel),
+                  },
+                })}
+              />
+              {errors.mobileNumber && (
+                <p
+                  role="alert"
+                  className="mt-[-0.75rem] px-6 text-xs text-red-500"
+                >
+                  This field is required, and should be a valid local mobile
+                  number.
+                </p>
+              )}
+
+              <div className="mt-4 flex w-full flex-col gap-4">
+                <button
+                  type="submit"
+                  className="rounded-full bg-otb-blue px-6 py-4 font-semibold uppercase shadow-md transition-all hover:bg-black hover:text-white hover:shadow-none"
+                >
+                  Register
+                </button>
+
+                <span className="block text-center text-sm">
+                  Already have an account?{' '}
+                  <Link
+                    href="/login"
+                    className="font-bold text-otb-blue underline shadow-sm"
+                  >
+                    Login
+                  </Link>
+                </span>
+              </div>
+            </form>
+          ) : (
+            <OTPForm
+              handleSubmit={handleOTPSubmit}
+              handleChange={handleOTPChange}
+              otp={otp}
+            />
+          )}
         </div>
       </div>
     </div>
