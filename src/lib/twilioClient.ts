84737088cbdf1248f59f2464f3506a123335a4a6
kigos21 <decastrojohndaniel@gmail.com>
2024-02-04 15:37:54 +0800

    add send otp function

---
+++ src/lib/twilioClient.ts
@@ -0,0 +1,23 @@
+import { TwilioCreateError } from '@/app/classes';
+import twilioClient from 'twilio';
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID as string;
+const authToken = process.env.TWILIO_AUTH_TOKEN as string;
+const verifySid = process.env.TWILIO_VERIFY_SID as string;
+
+export const twilio = twilioClient(accountSid, authToken);
+
+export async function sendOTP(mobileNumber: string) {
+  try {
+    const verification = await twilio.verify.v2
+      .services(verifySid)
+      .verifications.create({ to: mobileNumber, channel: 'sms' });
+
+    return verification;
+  } catch (error) {
+    console.log('TWILIO ERROR: ' + error);
+    throw new TwilioCreateError(
+      'There was an error sending an OTP to your number.',
+    );
+  }
+}
