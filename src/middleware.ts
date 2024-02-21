aee040b17b6228b1f1bc8ade612257aaab97ed5b
SenseiEA <nandacansino@gmail.com>
2024-02-21 10:20:02 +0800

    noel-branch update on login/logout changes
    install npm react-cookie
    login/logout button change on user login or logout
    database integration

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,6 +8,15 @@ export function middleware(req: NextRequest) {
     // Respond with an empty cookie with the same name and maxAge set to 0
     const response = NextResponse.next();
     response.cookies.set('token', '', { maxAge: 0 });
+
+    // Set a header to instruct the browser to remove the cookie
+    response.headers.set(
+      'Set-Cookie',
+      'token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT;',
+    );
+
+    // Force page reload after logout
+    response.headers.set('Refresh', '0;url=/'); // Redirect to homepage
     return response;
   }
 
