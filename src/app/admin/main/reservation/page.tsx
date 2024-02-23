12daa2bb5776b0885be8bc37cee37ac57a3cc969
Kevin Coraza <kevcoraza@gmail.com>
2024-02-23 11:56:23 +0800

    small change
    small

--- src/app/admin/main/reservation/page.tsx
+++ src/app/admin/main/reservation/page.tsx
@@ -1,15 +1,19 @@
 export default function Reservation() {
   return (
-    <div className="flex flex-col h-full space-y-5">
-      <div className="basis-5/6 basis bg-white rounded-lg px-5 py-7 shadow-lg shadow-black/25">
+    <div className="flex h-full flex-col space-y-5">
+      <div className="basis basis-5/6 rounded-lg bg-white px-5 py-7 shadow-lg shadow-black/25">
         test
       </div>
-      <div className="flex basis-1/6 bg-white rounded-lg px-5 py-7 shadow-lg shadow-black/25">
-        <div className="flex flex-nowrap place-content-between w-full">
-          <h3 className="text-5xl font-semibold self-center">Reserve the Facility</h3>
-          <button className="rounded-lg bg-otb-blue shadow-lg shadow-black/25 px-10 py-2 h-min text-lg font-semibold self-center">Proceed</button>
+      <div className="flex basis-1/6 rounded-lg bg-white px-5 py-7 shadow-lg shadow-black/25">
+        <div className="flex w-full flex-nowrap place-content-between">
+          <h3 className="self-center text-5xl font-semibold">
+            Reserve the Facility
+          </h3>
+          <button className="h-min self-center rounded-lg bg-otb-blue px-10 py-2 text-lg font-semibold shadow-lg shadow-black/25">
+            Proceed
+          </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/app/admin/main/services/page.tsx
+++ src/app/admin/main/services/page.tsx
@@ -1,7 +1,51 @@
 export default function Services() {
+  const testData = [
+    {
+      key: 1,
+      service: '2hrsReg',
+      hours: '2',
+      price: '149.00',
+      servicetype: 'Regular',
+    },
+    {
+      key: 2,
+      service: '2hrsRegDT',
+      hours: '2',
+      price: '174.00',
+      servicetype: 'Regular',
+    },
+  ];
+
   return (
-    <div className="h-full rounded-lg bg-white px-5 py-7 shadow-lg shadow-black/25">
-      services page
+    <div className="flex h-full flex-col space-y-5">
+      <div className="h-full rounded-lg bg-white px-5 py-7 shadow-lg shadow-black/25">
+        <h3 className="mb-3 text-xl font-semibold">
+          Manage Services and Promos
+        </h3>
+        <table className="w-full table-fixed text-center">
+          <thead className="">
+            <tr>
+              <th>SERVICE / PROMO</th>
+              <th>HOURS</th>
+              <th>PRICE</th>
+              <th>SERVICE TYPE</th>
+            </tr>
+          </thead>
+          <tbody>
+            {testData.map((data) => (
+              <tr
+                className="h-9 border border-solid border-black"
+                key={data.key}
+              >
+                <td>{data.service}</td>
+                <td>{data.hours}</td>
+                <td>{data.price}</td>
+                <td>{data.servicetype}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
