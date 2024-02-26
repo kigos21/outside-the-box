865a4c0cff999d7352465af943791e4ddc0db91e
Kevin Coraza <kevcoraza@gmail.com>
2024-02-26 09:47:42 +0800

    change
    2

--- src/app/admin/main/services/AddButton.js
+++ src/app/admin/main/services/AddButton.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import Link from 'next/link';
 
 const AddButton = ({ IconComponent, color = 'white' }) => {
   return (
-    <button>
-      <div
-        className={`flex items-center justify-center rounded-lg bg-sky-300 p-2 text-white shadow-lg ${color}`}
-      >
-        <IconComponent className="h-8 w-8" />
-      </div>
-    </button>
+    <Link href="/admin/main/services/addservices">
+      {' '}
+      <a>
+        <button>
+          <div
+            className={`flex items-center justify-center rounded-lg bg-sky-300 p-2 text-white shadow-lg ${color}`}
+          >
+            <IconComponent className="h-8 w-8" />
+          </div>
+        </button>
+      </a>
+    </Link>
   );
 };
 
--- src/app/admin/main/services/addservices/page.tsx
+++ src/app/admin/main/services/addservices/page.tsx
@@ -2,10 +2,48 @@
 import { useForm } from 'react-hook-form';
 
 export default function AddServices() {
+  const form = useForm();
   return (
     <div className="flex h-full flex-col space-y-5">
       <div className="h-full rounded-lg bg-white px-5 py-7 shadow-lg shadow-black/25">
-        <h3 className="mb-3 text-xl font-semibold">New Service / Promo</h3>
+        <div className="ml-6">
+          <h3 className="mb-14 mt-7 text-4xl font-semibold">
+            New Service / Promo
+          </h3>
+          <div>
+            <form className="flex flex-col gap-12">
+              <div>
+                <label className="mb-2 block text-xl font-medium text-black ">
+                  SERVICE / PROMO NAME
+                </label>
+                <input type="text" className="w-1/2 outline outline-2" />
+              </div>
+              <div>
+                <label className="mb-2 block text-xl font-medium text-black ">
+                  HOURS
+                </label>
+                <input type="text" className="w-1/2 outline outline-2" />
+              </div>
+              <div>
+                <label className="mb-2 block text-xl font-medium text-black ">
+                  PRICE
+                </label>
+                <input type="text" className="w-1/2 outline outline-2" />
+              </div>
+              <div>
+                <label className="mb-2 block text-xl font-medium text-black ">
+                  SERVICE TYPE
+                </label>
+                <input type="text" className="w-1/2 outline outline-2" />
+              </div>
+            </form>
+            <div className="float-right mr-20 mt-40">
+              <button className="rounded-lg bg-otb-blue px-10 py-2 text-lg font-semibold shadow-lg shadow-black/25">
+                Add Service
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
