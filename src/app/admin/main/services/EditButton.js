73785baeda2aafe6735b928d2e84d7af1e76e4c4
Kevin Coraza <kevcoraza@gmail.com>
2024-02-26 09:54:08 +0800

    edit
    edit

--- src/app/admin/main/services/EditButton.js
+++ src/app/admin/main/services/EditButton.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import Link from 'next/link';
 
 const EditButton = ({ IconComponent, color = 'white' }) => {
   return (
-    <button>
-      <div
-        className={`flex items-center justify-center rounded-lg bg-blue-700 p-2 text-white shadow-lg ${color}`}
-      >
-        <IconComponent className="h-5 w-5" />
-      </div>
-    </button>
+    <Link href="/admin/main/services/editservices">
+      <button>
+        <div
+          className={`flex items-center justify-center rounded-lg bg-blue-700 p-2 text-white shadow-lg ${color}`}
+        >
+          <IconComponent className="h-5 w-5" />
+        </div>
+      </button>
+    </Link>
   );
 };
 
