210729eb3f79638daaa79941785563172e0810ec
TaculaIT <karl.tacula.cics@ust.edu.ph>
2024-02-27 08:02:06 +0800

    link added

--- src/app/admin/(login)/forgot-password/page.tsx
+++ src/app/admin/(login)/forgot-password/page.tsx
@@ -118,13 +118,14 @@ function NewPassword() {
           placeholder="New Password"
           className="mt-3 w-full rounded-full border border-gray-300 px-6 py-4 text-center text-lg"
         />
-
-        <button
-          type="submit"
-          className="my-4 w-full rounded-full bg-otb-blue px-6 py-4 font-semibold uppercase shadow-md transition-all hover:bg-black hover:text-white hover:shadow-none"
-        >
-          Reset
-        </button>
+        <Link href="/admin" passHref>
+          <button
+            type="submit"
+            className="my-4 w-full rounded-full bg-otb-blue px-6 py-4 font-semibold uppercase shadow-md transition-all hover:bg-black hover:text-white hover:shadow-none"
+          >
+            Reset
+          </button>
+        </Link>
       </form>
     </div>
   );
