809530a94a1dbe890b0e47afca79f8796045d4df
kigos21 <decastrojohndaniel@gmail.com>
2024-03-02 16:58:31 +0800

    optimize style coherence

--- src/app/admin/main/reservation/page.tsx
+++ src/app/admin/main/reservation/page.tsx
@@ -42,10 +42,12 @@ export default function Reservation() {
     },
   ];
   return (
-    <div className="flex h-full flex-col space-y-5">
-      <div className="basis basis-5/6 rounded-lg bg-white px-5 py-7 shadow-lg shadow-black/25">
-        <h3 className="mb-3 text-3xl font-semibold">Book Reservations</h3>
-        <h3 className="mb-3 px-4 text-2xl font-semibold">
+    <div className="flex h-full flex-col gap-5">
+      <div className="flex-grow rounded-lg bg-white px-8 py-6 shadow-lg shadow-black/25">
+        <h3 className="absolute top-10 text-3xl font-bold">
+          Book Reservations
+        </h3>
+        <h3 className="mb-3 text-xl font-semibold">
           Reservations for Confirmation
         </h3>
         <table className="relative w-full table-fixed text-center">
@@ -112,9 +114,9 @@ export default function Reservation() {
         </table>
       </div>
 
-      <div className="flex basis-1/6 rounded-lg bg-white px-5 py-7 shadow-lg shadow-black/25">
-        <div className="flex w-full flex-nowrap place-content-between">
-          <h3 className="self-center text-5xl font-semibold">
+      <div className="flex rounded-lg bg-white px-8 py-6 shadow-lg shadow-black/25">
+        <div className="flex w-full justify-between">
+          <h3 className="self-center text-xl font-semibold">
             Reserve the Facility
           </h3>
           <button className="h-min self-center rounded-lg bg-otb-blue px-10 py-2 text-lg font-semibold shadow-lg shadow-black/25">
