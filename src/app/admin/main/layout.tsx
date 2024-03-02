aa59b3cd6e4e67f7c928d89bbcf2f157e2286589
kigos21 <decastrojohndaniel@gmail.com>
2024-03-02 16:26:51 +0800

    modified structure

--- src/app/admin/main/layout.tsx
+++ src/app/admin/main/layout.tsx
@@ -3,6 +3,9 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import AdminHeader from '@/components/AdminHeader';
 
+import Link from 'next/link';
+import Image from 'next/image';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -15,13 +18,22 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-
   return (
     <html lang="en">
-      <body className={`${inter.className} flex min-h-screen flex-row bg-otb-yellow px-10 py-20 space-x-5` }
-      >
-        <AdminHeader />
-        <main className='basis-4/5'>{children}</main>
+      <body className={`${inter.className} min-h-screen bg-otb-yellow p-8`}>
+        <Link href={'/admin/main'} className="block w-fit">
+          <Image
+            src="/otb-logo-cropped.jpg"
+            width={160}
+            height={57}
+            alt="Outside the Box Logo"
+            className="mb-4"
+          />
+        </Link>
+        <div className="flex gap-5">
+          <AdminHeader />
+          <main className="basis-4/5">{children}</main>
+        </div>
       </body>
     </html>
   );
