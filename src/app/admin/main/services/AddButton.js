c08a2cb11e6116486902940fcb8962328abdc568
kigos21 <decastrojohndaniel@gmail.com>
2024-03-03 07:34:21 +0800

    /admin/main/logs page

--- src/app/admin/main/services/AddButton.js
+++ src/app/admin/main/services/AddButton.js
@@ -4,16 +4,13 @@ import Link from 'next/link';
 const AddButton = ({ IconComponent, color = 'white' }) => {
   return (
     <Link href="/admin/main/services/addservices">
-      {' '}
-      <a>
-        <button>
-          <div
-            className={`flex items-center justify-center rounded-lg bg-sky-300 p-2 text-white shadow-lg ${color}`}
-          >
-            <IconComponent className="h-8 w-8" />
-          </div>
-        </button>
-      </a>
+      <button>
+        <div
+          className={`flex items-center justify-center rounded-lg bg-sky-300 p-2 text-white shadow-lg ${color}`}
+        >
+          <IconComponent className="h-8 w-8" />
+        </div>
+      </button>
     </Link>
   );
 };
