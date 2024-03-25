70bec47f92bab5f9d5e9fd04cfa6f41197903a27
kigos21 <decastrojohndaniel@gmail.com>
2024-03-25 18:25:12 +0800

    log records create, read, create from reservation

--- src/app/admin/main/logs/add-log/page.tsx
+++ src/app/admin/main/logs/add-log/page.tsx
@@ -1,82 +1,84 @@
 'use client';
 
-import AdminModal from '@/components/AdminModal';
 import { useEffect, useState } from 'react';
-import fetch from 'node-fetch';
+import { CreateLogRequestBody } from '@/types';
 
 export default function AddLog() {
+  const [formData, setFormData] = useState<CreateLogRequestBody>({
+    firstName: '',
+    lastName: '',
+    serviceId: '',
+  });
+
+  const [services, setServices] = useState<any[]>([]);
+  const [message, setMessage] = useState('');
   const [showModal, setShowModal] = useState(false);
-  const [timeString, setTimeString] = useState('');
-  const [services, setServices] = useState<any[]>([]); //initialize state for services
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [selectedService, setSelectedService] = useState('');
-
-  useEffect(() => {
-    const date = new Date();
-    const hour = date.getHours().toString();
-    const minutes = date.getMinutes().toString();
-
-    setTimeString(`${hour}:${minutes}`);
-  }, [showModal]);
-
-  const fetchServices = async () => {
-    try {
-      const response = await fetch('/api/services');
-      const data = await response.json();
-
-      if (data.success) {
-        setServices(data.services);
-      } else {
-        console.error('Error fetching services', data.error);
-      }
-    } catch (error) {
-      console.error('Error fetching services:', error);
-    }
-  };
 
   useEffect(() => {
     fetchServices();
   }, []);
 
-  const handleAddRecord = () => {
-    setShowModal(true);
-    setSelectedService(selectedService);
-    setFirstName(firstName);
-    setLastName(lastName);
+  const fetchServices = async () => {
+    const response = await fetch('/api/services');
+
+    if (response.ok) {
+      const { services } = await response.json();
+      setServices(services);
+    } else {
+      const message = await response.text();
+      console.error(message);
+      alert(message);
+    }
   };
+
+  const handleSubmit = async (formData: CreateLogRequestBody) => {
+    const response = await fetch('/api/logs/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const { message } = await response.json();
+
+    if (response.ok) {
+      setMessage(message);
+      setShowModal(true);
+      setFormData({
+        firstName: '',
+        lastName: '',
+        serviceId: '',
+      });
+    } else {
+      console.log(message);
+      alert(message);
+    }
+  };
+
+  const modal = (
+    <div className="absolute bottom-0 left-0 right-0 top-0 z-10 flex items-center justify-center bg-black/75">
+      <div className="mb-12 flex w-[28rem] flex-col gap-8 rounded-lg bg-white px-8 py-6">
+        <p className="rounded-md border border-green-700 bg-green-100 px-4 py-2 text-xl font-bold text-green-700">
+          Success
+        </p>
+
+        <div className="mt-[-1rem] text-sm text-gray-500">{message}</div>
+
+        <div className="flex">
+          <button
+            onClick={() => setShowModal(false)}
+            className={`basis-full rounded-md bg-otb-blue px-6 py-4 font-semibold uppercase shadow-md transition-all hover:bg-black hover:text-white hover:shadow-none`}
+          >
+            OK
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 
   return (
     <div className="flex h-full flex-col">
-      {showModal && (
-        <AdminModal
-          title={'Create log based on this reservation?'}
-          handleConfirm={() => console.log('Confirm')}
-          handleCancel={() => {
-            setShowModal(false);
-          }}
-        >
-          <div className="flex flex-col gap-1">
-            <div className="flex justify-between gap-4">
-              <p className="basis-1/2">First name</p>
-              <p className="basis-1/2 font-semibold">{firstName}</p>
-            </div>
-            <div className="flex justify-between gap-4">
-              <p className="basis-1/2">Last name</p>
-              <p className="basis-1/2 font-semibold">{lastName}</p>
-            </div>
-            <div className="flex justify-between gap-4">
-              <p className="basis-1/2">Service</p>
-              <p className="basis-1/2 font-semibold">{selectedService}</p>
-            </div>
-            <div className="flex justify-between gap-4">
-              <p className="basis-1/2">Time in</p>
-              <p className="basis-1/2 font-semibold">{timeString}</p>
-            </div>
-          </div>
-        </AdminModal>
-      )}
-
       <div className="flex h-full flex-col rounded-lg bg-white px-8 py-6 shadow-lg shadow-black/25">
         <h3 className="absolute top-10 text-3xl font-bold">Add Log</h3>
 
@@ -84,45 +86,61 @@ export default function AddLog() {
 
         <form
           className="flex flex-grow flex-col justify-between"
-          onSubmit={(e) => e.preventDefault()}
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit(formData);
+          }}
         >
           <div className="flex flex-col gap-4">
             <div className="flex items-center gap-4">
-              <label htmlFor="" className="basis-2/12">
+              <label htmlFor="firstName" className="basis-2/12">
                 First name
               </label>
               <input
                 type="text"
-                name=""
-                id=""
+                name="firstName"
+                id="firstName"
+                required
                 className="basis-3/12 rounded-md border border-gray-400 px-6 py-4"
-                onChange={(e) => setFirstName(e.target.value)}
+                value={formData.firstName}
+                onChange={(e) =>
+                  setFormData({ ...formData, firstName: e.target.value })
+                }
               />
             </div>
             <div className="flex items-center gap-4">
-              <label htmlFor="" className="basis-2/12">
+              <label htmlFor="lastName" className="basis-2/12">
                 Last name
               </label>
               <input
                 type="text"
-                name=""
-                id=""
+                name="lastName"
+                id="lastName"
+                required
                 className="basis-3/12 rounded-md border border-gray-400 px-6 py-4"
-                onChange={(e) => setLastName(e.target.value)}
+                value={formData.lastName}
+                onChange={(e) =>
+                  setFormData({ ...formData, lastName: e.target.value })
+                }
               />
             </div>
             <div className="flex items-center gap-4">
-              <label htmlFor="" className="basis-2/12">
+              <label htmlFor="service" className="basis-2/12">
                 Service
               </label>
               <select
-                name=""
-                id=""
+                name="service"
+                id="service"
+                required
                 className="basis-3/12 rounded-md border border-gray-400 px-6 py-4"
-                onChange={(e) => setSelectedService(e.target.value)}
+                value={formData.serviceId}
+                onChange={(e) =>
+                  setFormData({ ...formData, serviceId: e.target.value })
+                }
               >
+                <option value="" hidden></option>
                 {services.map((service) => (
-                  <option value={service.name} key={service.name}>
+                  <option value={service.id} key={service.id}>
                     {service.name}
                   </option>
                 ))}
@@ -131,13 +149,15 @@ export default function AddLog() {
           </div>
 
           <button
+            type="submit"
             className="w-fit self-end rounded-md bg-otb-blue px-6 py-4 font-semibold uppercase shadow-md transition-all hover:bg-black hover:text-white hover:shadow-none"
-            onClick={handleAddRecord}
           >
             Add Record
           </button>
         </form>
       </div>
+
+      {showModal && modal}
     </div>
   );
 }
