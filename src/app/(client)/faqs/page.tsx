cd8c81b837aeb2d7c1e3e986417ade965ea845ef
TaculaIT <karl.tacula.cics@ust.edu.ph>
2024-04-10 10:44:14 +0800

    link footer added

--- src/app/(client)/faqs/page.tsx
+++ src/app/(client)/faqs/page.tsx
@@ -1,7 +1,7 @@
-export default function Terms() {
+export default function page() {
   return (
     <div className="p-4">
-      <h1 className="mb-4 text-2xl font-bold">Terms and Conditions</h1>
+      <h1 className="mb-4 text-2xl font-bold">FAQs</h1>
       {/* Content of the Terms and Conditions page */}
     </div>
   );
---
+++ src/app/(client)/privacy/page.tsx
@@ -0,0 +1,8 @@
+export default function page() {
+  return (
+    <div className="p-4">
+      <h1 className="mb-4 text-2xl font-bold">Privacy</h1>
+      {/* Content of the Terms and Conditions page */}
+    </div>
+  );
+}
