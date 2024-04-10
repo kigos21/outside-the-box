81ed7cfb0cebfeff68a5a35b9ff4b3d37d74dddf
Kevin Coraza <kevcoraza@gmail.com>
2024-04-10 12:24:32 +0800

    faqs,priv,terms
    aaa

--- src/app/(client)/privacy/page.tsx
+++ src/app/(client)/privacy/page.tsx
@@ -1,8 +1,58 @@
 export default function page() {
   return (
-    <div className="p-4">
-      <h1 className="mb-4 text-2xl font-bold">Privacy</h1>
-      {/* Content of the Terms and Conditions page */}
+    <div className="mx-auto flex max-w-screen-lg flex-col gap-2 px-[7%] py-16 pt-28 font-sans xl:px-0">
+      <h1 className="mb-6 text-center font-sans text-6xl font-bold">
+        Privacy policy
+      </h1>
+      <h2 className="text-2xl font-bold">
+        How We Collect and Use Your Personal Information
+      </h2>
+      <p className="mb-4 text-left text-xl">
+        To provide the Services, we collect and have collected over the past 12
+        months personal information about you from a variety of sources, as set
+        out below. The information that we collect and use varies depending on
+        how you interact with us. In addition to the specific uses set out
+        below, we may use information we collect about you to communicate with
+        you, provide the Services, comply with any applicable legal obligations,
+        enforce any applicable terms of service, and to protect or defend the
+        Services, our rights, and the rights of our users or others.
+      </p>
+      <h2 className="text-2xl font-bold">
+        What Personal Information We Collect
+      </h2>
+      <p className="mb-4 text-left text-xl">
+        The types of personal information we obtain about you depends on how you
+        interact with our Site and use our Services. When we use the term
+        "personal information", we are referring to information that identifies,
+        relates to, describes or can be associated with you. The following
+        sections describe the categories and specific types of personal
+        information we collect.
+      </p>
+      <h2 className="text-2xl font-bold">SECTION 2</h2>
+      <p className="mb-4 text-left text-xl">
+        Para pumasa ay di ko na alam dahil ang mga panelists ang sasagot sa atin
+        pag dating sa ganyanan kaya pray to God Almighty. Lord please help us in
+        this project and always guide us. Para pumasa ay di ko na alam dahil ang
+        mga panelists ang sasagot sa atin pag dating sa ganyanan kaya pray to
+        God Almighty. Lord please help us in this project and always guide us.
+        Para pumasa ay di ko na alam dahil ang mga panelists ang sasagot sa atin
+        pag dating sa ganyanan kaya pray to God Almighty. Lord please help us in
+        this project and always guide us. Para pumasa ay di ko na alam dahil ang
+        mga panelists ang sasagot sa atin pag dating sa ganyanan kaya pray to
+        God Almighty. Lord please help us in this project and always guide us.
+      </p>
+      <h2 className="text-2xl font-bold">Paano nga ba pumasa sa sub na ito?</h2>
+      <p className="mb-4 text-left text-xl">
+        Para pumasa ay di ko na alam dahil ang mga panelists ang sasagot sa atin
+        pag dating sa ganyanan kaya pray to God Almighty. Lord please help us in
+        this project and always guide us.
+      </p>
+      <h2 className="text-2xl font-bold">Paano nga ba pumasa sa sub na ito?</h2>
+      <p className="mb-4 text-left text-xl">
+        Para pumasa ay di ko na alam dahil ang mga panelists ang sasagot sa atin
+        pag dating sa ganyanan kaya pray to God Almighty. Lord please help us in
+        this project and always guide us.
+      </p>
     </div>
   );
 }
