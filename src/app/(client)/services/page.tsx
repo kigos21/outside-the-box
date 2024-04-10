48997a25c62a0053db5b27ef771469c081fa9dbf
Kevin Coraza <kevcoraza@gmail.com>
2024-04-10 08:00:12 +0800

    can't find link
    aaaaaaaaaa

--- src/app/(client)/services/page.tsx
+++ src/app/(client)/services/page.tsx
@@ -3,7 +3,7 @@ import Reservation from '@/components/services/Reservation';
 
 export default function Page() {
   return (
-    <div className="max-w-7x2 mx-auto mt-24 flex flex-col gap-8 bg-cs-cream px-[7%] py-16">
+    <div className="max-w-7x2 mx-auto flex flex-col gap-8 bg-cs-cream px-[7%] py-16 pt-32">
       <OfferedServices />
       <Reservation />
     </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,23 @@
+import Link from 'next/link';
+
 export default function Footer() {
   return (
     <div>
       <div className="h-0.5 w-full bg-[#a6a6a6]" />
       <div className="mt-auto flex flex-col items-start justify-between gap-2 bg-cs-cream px-4 py-6 md:flex-row md:px-[7%] md:py-10">
-        <div className=" grid grid-cols-1 md:grid-cols-2">
+        <div className="grid grid-cols-1 md:grid-cols-2">
           <div className="mb-0 flex w-full flex-col items-start md:mb-0 ">
-            <p className=" text-sm font-bold">Terms and Conditions</p>
-            <p className="text-sm font-bold">Privacy policy</p>
+            <Link href="/pages/Terms">
+              <span className="text-sm font-bold">Terms and Conditions</span>
+            </Link>
+            <Link href="Privacy">
+              <span className="text-sm font-bold">Privacy policy</span>
+            </Link>
           </div>
           <div className="mb-4 ml-0 flex w-full flex-col items-start md:mb-0 md:ml-10 ">
-            <p className="text-sm font-bold">FAQs</p>
+            <Link href="FAQs">
+              <span className="text-sm font-bold">FAQs</span>
+            </Link>
           </div>
         </div>
         <div className="mb-4 grid grid-cols-3 items-start gap-6 md:gap-8">
