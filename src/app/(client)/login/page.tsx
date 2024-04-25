4d84df80c16fa9ccb483a55c9b22c932de527c0e
Kevin Coraza <kevcoraza@gmail.com>
2024-04-25 12:03:42 +0800

    hey
    a

--- src/app/(client)/login/page.tsx
+++ src/app/(client)/login/page.tsx
@@ -12,6 +12,11 @@ export default function Login() {
   const [tries, setTries] = useState<number>(0);
   const [timeout, setTimeout] = useState<number>(0);
   const [timeoutMessage, setTimeoutMessage] = useState<string>('');
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
 
   useEffect(() => {
     setTimeoutMessage(`Try again after ${timeout} seconds.`);
@@ -89,7 +94,7 @@ export default function Login() {
 
   return (
     <div className="flex min-h-[85dvh] items-center justify-center px-4 py-16 pt-40">
-      <div className="bg-cs-yellow flex flex-col items-center justify-center gap-8 rounded-lg px-8 py-6 shadow-2xl max-sm:flex-grow sm:px-16 sm:py-12">
+      <div className="flex flex-col items-center justify-center gap-8 rounded-lg bg-cs-yellow px-8 py-6 shadow-2xl max-sm:flex-grow sm:px-16 sm:py-12">
         {/* Sa Login errors to */}
         {errorMessage && (
           <div className="w-full rounded-md border border-red-400 bg-red-50 p-4 text-center text-sm text-red-600">
@@ -134,14 +139,60 @@ export default function Login() {
               </p>
             )}
 
-            <input
-              type="password"
-              id="password"
-              required
-              placeholder="Password"
-              className="rounded-full border border-gray-300 px-6 py-4"
-              {...register('password', { required: true })}
-            />
+            <div className="relative">
+              <input
+                type={passwordVisible ? 'text' : 'password'}
+                id="password"
+                required
+                placeholder="Password"
+                className="w-full rounded-full border border-gray-300 px-6 py-4"
+                {...register('password', { required: true })}
+              />
+
+              <button
+                type="button"
+                onClick={togglePasswordVisibility}
+                className="absolute inset-y-0 right-0 flex items-center pr-4 text-sm leading-5"
+              >
+                {passwordVisible ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="h-6 w-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="h-6 w-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+
             {errors.password && (
               <p
                 role="alert"
@@ -169,7 +220,7 @@ export default function Login() {
             <div className="mt-0 flex w-full flex-col items-center gap-4">
               <button
                 disabled={timeout !== 0}
-                className="bg-cs-blue text-cs-cream hover:bg-cs-black hover:text-cs-cream w-28 rounded-lg px-6 py-4 font-semibold uppercase shadow-md transition-all hover:shadow-none"
+                className="w-28 rounded-lg bg-cs-blue px-6 py-4 font-semibold uppercase text-cs-cream shadow-md transition-all hover:bg-cs-black hover:text-cs-cream hover:shadow-none"
               >
                 Login
               </button>
@@ -178,7 +229,7 @@ export default function Login() {
                 Don&apos;t have an account yet?{' '}
                 <Link
                   href="/register"
-                  className="text-cs-blue font-bold underline shadow-sm"
+                  className="font-bold text-cs-blue underline shadow-sm"
                 >
                   Register
                 </Link>{' '}
--- src/app/(client)/register/page.tsx
+++ src/app/(client)/register/page.tsx
@@ -14,6 +14,15 @@ export default function Register() {
   const [responseError, setResponseErrors] = useState<string>('');
   const [registerError, setRegisterError] = useState<string>('');
   const [isEnteringOTP, setIsEnteringOTP] = useState(true);
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+  const toggleConfirmPasswordVisibility = () => {
+    setConfirmPasswordVisible(!confirmPasswordVisible);
+  };
 
   const {
     register,
@@ -138,23 +147,68 @@ export default function Register() {
                 </p>
               )}
 
-              <input
-                type="password"
-                id="password"
-                required
-                placeholder="Password"
-                className="rounded-lg border border-gray-300 px-6 py-4"
-                {...register('password', {
-                  required: true,
-                  minLength: 8,
-                  validate: {
-                    strength: (input) =>
-                      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
-                        input,
-                      ),
-                  },
-                })}
-              />
+              <div className="relative">
+                <input
+                  type={passwordVisible ? 'text' : 'password'}
+                  id="password"
+                  required
+                  placeholder="Password"
+                  className="w-full rounded-lg border border-gray-300 px-6 py-4"
+                  {...register('password', {
+                    required: true,
+                    minLength: 8,
+                    validate: {
+                      strength: (input) =>
+                        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+                          input.trim(),
+                        ),
+                    },
+                  })}
+                />
+                <button
+                  type="button"
+                  onClick={togglePasswordVisibility}
+                  className="absolute inset-y-0 right-0 flex items-center pr-4 text-sm leading-5"
+                >
+                  {passwordVisible ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="h-6 w-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="h-6 w-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+                      />
+                    </svg>
+                  )}
+                </button>
+              </div>
+
               {errors.password && (
                 <p
                   role="alert"
@@ -166,19 +220,64 @@ export default function Register() {
                 </p>
               )}
 
-              <input
-                type="password"
-                id="confirmPassword"
-                required
-                placeholder="Confirm Password"
-                className="rounded-lg border border-gray-300 px-6 py-4"
-                {...register('confirmPassword', {
-                  required: true,
-                  validate: {
-                    match: (input) => input === watch('password'),
-                  },
-                })}
-              />
+              <div className="relative">
+                <input
+                  type={confirmPasswordVisible ? 'text' : 'password'}
+                  id="confirmPassword"
+                  required
+                  placeholder="Confirm Password"
+                  className="w-full rounded-lg border border-gray-300 px-6 py-4"
+                  {...register('confirmPassword', {
+                    required: true,
+                    validate: {
+                      match: (input) => input === watch('password'),
+                    },
+                  })}
+                />
+                <button
+                  type="button"
+                  onClick={toggleConfirmPasswordVisibility}
+                  className="absolute inset-y-0 right-0 flex items-center pr-4 text-sm leading-5"
+                >
+                  {confirmPasswordVisible ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="h-6 w-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="h-6 w-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+                      />
+                    </svg>
+                  )}
+                </button>
+              </div>
+
               {errors.confirmPassword?.type === 'required' && (
                 <p
                   role="alert"
