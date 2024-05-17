ebbbc47bd2f60b5201d4f269fcac81c196e3c622
kigos21 <decastrojohndaniel@gmail.com>
2024-05-18 00:45:55 +0800

    fix types

--- src/app/api/admin/reservation/archive/route.ts
+++ src/app/api/admin/reservation/archive/route.ts
@@ -3,9 +3,11 @@ import { prismaClient } from '@/lib/prismaClient';
 export async function POST(req: Request) {
   const { id } = await req.json();
 
+  const intId = parseInt(id, 10);
+
   try {
     const archivedReservation = await prismaClient.archivedReservation.create({
-      data: { seatReservationId: id },
+      data: { seatReservationId: intId },
     });
 
     return Response.json({ archivedReservation }, { status: 200 });
