9174c333fbcbbcbcfcd72b09a912c643030a1336
kigos21 <decastrojohndaniel@gmail.com>
2024-05-19 16:14:01 +0800

    fix middleware redirections

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,50 +1,87 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
 
-// This function can be marked `async` if using `await` inside
-export async function middleware(req: NextRequest) {
-  const path = req.nextUrl.pathname;
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
 
-  if (path === '/login' || path === '/register') {
-    if (req.cookies.has('token')) {
-      return NextResponse.redirect(new URL('/', req.url));
-    } else if (req.cookies.has('adminToken')) {
-      return NextResponse.redirect(new URL('/admin/main', req.url));
-    }
-  } else if (path.startsWith('/reservation')) {
-    // check if token is present in cookies
-    if (!req.cookies.has('token')) {
-      return NextResponse.redirect(new URL('/login', req.url));
-    }
+  const token = request.cookies.get('token');
+  const adminToken = request.cookies.get('adminToken');
+
+  // Handle /login and /register redirections
+  if (
+    (pathname === '/login' || pathname === '/register') &&
+    (token || adminToken)
+  ) {
+    return NextResponse.redirect(new URL('/', request.url));
   }
 
-  // admin-side middleware logic
-  else if (path === '/admin') {
-    if (req.cookies.has('adminToken')) {
-      return NextResponse.redirect(new URL('/admin/main', req.url));
-    } else if (req.cookies.has('token')) {
-      return NextResponse.redirect(new URL('/', req.url));
-    }
-  } else if (path.startsWith('/admin/main')) {
-    if (!req.cookies.has('adminToken')) {
-      return NextResponse.redirect(new URL('/login', req.url));
-    }
+  // Handle /reservation/* redirections
+  if (pathname.startsWith('/reservation') && !token) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  // Handle /admin/* redirections
+  if (pathname.startsWith('/admin') && !adminToken) {
+    return NextResponse.redirect(new URL('/login', request.url));
   }
 
   return NextResponse.next();
 }
 
-// ADD HERE PROTECTED PATHS
-// matcher can accept regexp of pathname
 export const config = {
   matcher: [
-    // client-side
     '/login',
     '/register',
-    '/logout',
     '/reservation/:path*',
-
-    // admin-side
     '/admin',
     '/admin/main/:path*',
   ],
 };
+
+// import { NextRequest, NextResponse } from 'next/server';
+
+// // This function can be marked `async` if using `await` inside
+// export async function middleware(req: NextRequest) {
+//   const path = req.nextUrl.pathname;
+
+//   console.log(JSON.stringify({ reqUrl: req.url }));
+
+//   if (path === '/login' || path === '/register') {
+//     if (req.cookies.has('token')) {
+//       return NextResponse.redirect(new URL('/services', req.url));
+//     } else if (req.cookies.has('adminToken')) {
+//       return NextResponse.redirect(new URL('/admin/main', req.url));
+//     }
+//   } else if (path.startsWith('/reservation')) {
+//     // check if token is present in cookies
+//     if (!req.cookies.has('token')) {
+//       return NextResponse.redirect(new URL('/login', req.url));
+//     }
+//   }
+
+//   // admin-side middleware logic
+//   else if (path === '/admin') {
+//     return NextResponse.redirect(new URL('/login', req.url));
+//   } else if (path.startsWith('/admin/main')) {
+//     if (!req.cookies.has('adminToken')) {
+//       return NextResponse.redirect(new URL('/login', req.url));
+//     }
+//   }
+
+//   return NextResponse.next();
+// }
+
+// // ADD HERE PROTECTED PATHS
+// // matcher can accept regexp of pathname
+// export const config = {
+//   matcher: [
+//     // client-side
+//     '/login',
+//     '/register',
+//     '/reservation/:path*',
+
+//     // admin-side
+//     '/admin',
+//     '/admin/main/:path*',
+//   ],
+// };
